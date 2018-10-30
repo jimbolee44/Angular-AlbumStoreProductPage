4c898bd4917c52b4aed3381fd390d51f5cb08c7e
jimbolee44 <jimbolee44@gmail.com>
2018-10-30 16:47:47 -0500

    Add files via upload

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -17,7 +17,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
-  getProduct(): Observable<Product[]> {
+  getProducts(): Observable<Product[]> {
     return this._http.get(this._productUrl).map((response) => <Product[]>response.json());
   }
 
